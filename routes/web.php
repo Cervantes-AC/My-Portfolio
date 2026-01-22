e222f2a17c712726a364f6bb458dccc525686c0c
SharkAC <202120993+Cervantes-AC@users.noreply.github.com>
2026-01-23 06:41:48 +0800

    second commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,44 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home
+Route::get('/home', function () {
+return view('home');
+})->name('home');
+
+// About
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Aaron Clyde Cervantes',
+        'course' => 'Bachelor of Science in Information Technology',
+        'university' => 'Central Mindanao University',
+    ]);
+})->name('about');
+
+// Projects
+Route::get('/projects', function () {
+    $projects = [
+        [
+            'title' => 'Project 1',
+            'description' => 'My first web project',
+        ],
+        [
+            'title' => 'Project 2',
+            'description' => 'E-commerce website',
+        ],
+        [
+            'title' => 'Project 3',
+            'description' => 'Mobile app design',
+        ],
+    ];
+
+    return view('projects', compact('projects'));
+})->name('projects');
+
+// Contact
+Route::get('/contact', function () {
+    return view('contact', [
+        'email' => 's.cervantes.aaronclyde@cmu.edu.ph', // change this
+        'phone' => '+63 991 430 7358',        // change this if needed
+    ]);
+})->name('contact');
